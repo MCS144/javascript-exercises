c4e016d852280009566beb7940c9d8206441ef77
Matthew Slack <Matthew.Charles.Slack@gmail.com>
2022-11-02 11:38:11 -0400

    reverseString completed, all 4 test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+let splitString = str.split(""); //using split method to return a new array
+let reverseArray = splitString.reverse(); //reverse method to reverse the new array
+let joinArray = reverseArray.join(""); //join method to join them back together
 
+return joinArray; //return the reversed string
 };
 
 // Do not edit below this line
