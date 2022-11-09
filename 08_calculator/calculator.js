fc9c273ff21f5912a8c8e4410b9cb6a0331073d4
Matthew Slack <Matthew.Charles.Slack@gmail.com>
2022-11-09 11:31:50 -0500

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+  return  a + b;
+  
 	
 };
 
-const subtract = function() {
+const subtract = function(a1,b1) {
+  return a1 - b1;
+  
 	
 };
 
-const sum = function() {
-	
+const sum = function(sum1) {
+	return sum1.reduce((first, second) => first + second, 0);
 };
 
-const multiply = function() {
+const multiply = function(mult) {
+ return mult.length 
+ ? mult.reduce((first, second)=> first * second,) : 0;
+};
+
+const power = function(p1,p2) {
+return Math.pow(p1, p2)	
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  if (num < 0)
+  return -1;
+  else if (num == 0)
+  return 1;
+  else{
+    return (num *factorial(num-1));
+  }
 	
 };
 
